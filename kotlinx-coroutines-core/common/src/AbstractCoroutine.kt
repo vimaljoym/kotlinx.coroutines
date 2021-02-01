03e70d151f64ba6675b1fe692461471963a65719
Vsevolod Tolstopyatov <qwwdfsad@gmail.com>
2021-02-01 18:47:38 +0300

    Merge onStartInternal and onStart to reduce the number of methods and make code a bit simpler

--- kotlinx-coroutines-core/common/src/AbstractCoroutine.kt
+++ kotlinx-coroutines-core/common/src/AbstractCoroutine.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2016-2020 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
+ * Copyright 2016-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license.
  */
 @file:Suppress("DEPRECATION_ERROR")
 
@@ -63,16 +63,6 @@ public abstract class AbstractCoroutine<in T>(
      */
     internal fun initParentJob() {
         initParentJobInternal(parentContext[Job])
-    }
-
-    /**
-     * This function is invoked once when a non-active coroutine (constructed with `active` set to `false)
-     * is [started][start].
-     */
-    protected open fun onStart() {}
-
-    internal final override fun onStartInternal() {
-        onStart()
     }
 
     /**
